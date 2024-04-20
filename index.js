3a28cfd65d3e47cdb5205057cd19953c059d2a40
A K BORANA <88649979+akborana3@users.noreply.github.com>
2024-04-20 11:34:25 +0530

    Update index.js

--- index.js
+++ index.js
@@ -1,19 +1,12 @@
 const express = require('express');
 const app = express();
-const PORT = 3001;
-
-
 const cors = require('cors');
-
-app.use(cors());
-
-
 const routes = require('./Routes/telegramRoutes');
 
-app.use(express.json());
+require('dotenv').config();
 
+app.use(cors());
+app.use(express.json());
 app.use('/api', routes);
 
-app.listen(PORT, () => {
-    console.log(`Server is running on http://167.172.254.249:${PORT}`);
-});
+module.exports = app;
